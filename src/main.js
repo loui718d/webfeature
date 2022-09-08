96f2c42708055b8ccbda27f4125832ee5a0ffde7
Christian Hvid Ohm <chri29kx@stud.kea.dk>
2022-09-08 15:49:03 +0200

    kamera effekt

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
 import "./style.css";
+const body = document.querySelector("body");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 600 ||
+    document.documentElement.scrollTop > 600
+  ) {
+    body.classList.add("flicker-1");
+  } else {
+    body.classList.remove("flicker-1");
+  }
+}
